1a5e88cc43ee5aff76842c12fdf2151ebdfb645f
EMajesty <lassi.laitinen@protonmail.com>
2024-03-05 22:13:56 +0200

    vga text mode

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+mod vga_buffer;
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hell World");
+
+    panic!("SOS");
+    loop {}
+}
